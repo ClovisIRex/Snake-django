af16bf3ab2aceceb6e7d9e7c4e10784b7d8caebe
SolInvictus <livnytal.dev@gmail.com>
2017-05-19 14:55:09 +0300

    began writing models and added rest framework

--- snake/myfirsttime/models.py
+++ snake/myfirsttime/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Score(models.model):
+    player_name = models.CharField(max_length=15)
+    player_score = models.IntegerField()
+
+    
+
